dd312722df31bb2ea02cfda5328621564ae9dec2
Andy Newton <andy@arin.net>
2017-12-09 10:12:37 -0500

    a bunch of new tests

--- test.sh
+++ test.sh
@@ -4,11 +4,44 @@
 
 . assert.sh -v
 
+#
+# Test that the JCR is good itself
+#
 assert_raises "jcr -r jcr/rdap.jcr --test-jcr" 0
 
-for good_json in json/good/*.json
+#
+# Good JSON
+#
+
+# entities
+for good_json in json/good/entity_response/*.json
 do
-  assert_raises "jcr -r jcr/rdap.jcr $good_json" 0
+  assert_raises "jcr -r jcr/rdap.jcr -S entity_response $good_json" 0
 done
 
+# networks
+for good_json in json/good/network_response/*.json
+do
+  assert_raises "jcr -r jcr/rdap.jcr -S network_response $good_json" 0
+done
+
+# nameservers
+for good_json in json/good/nameserver_response/*.json
+do
+  assert_raises "jcr -r jcr/rdap.jcr -S nameserver_response $good_json" 0
+done
+
+#
+# Bad JSON
+#
+
+# networks
+for bad_json in json/bad/network_response/*.json
+do
+  assert_raises "jcr -r jcr/rdap.jcr -S network_response $bad_json" 3
+done
+
+#
+# Fini
+#
 assert_end
