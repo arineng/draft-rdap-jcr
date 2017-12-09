12284ce454b36701ba2c8035433b1b857f51fae9
Andy Newton <andy@arin.net>
2017-12-09 10:43:45 -0500

    added error responses

--- test.sh
+++ test.sh
@@ -43,6 +43,12 @@ do
   assert_raises "jcr -r jcr/rdap.jcr -S domain_response $good_json" 0
 done
 
+# errors
+for good_json in json/good/error_response/*.json
+do
+  assert_raises "jcr -r jcr/rdap.jcr -S error_response $good_json" 0
+done
+
 #
 # Bad JSON
 #
