4a700a305625374a2499281c91bfcaa671b151a3
andy <andy@arin.net>
2017-12-20 19:24:41 -0500

    trying to incorporate Mario's comments

--- xml2rfc.sh
+++ xml2rfc.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-draft=`grep docName draft-newton-rdap-jcr.xml | sed -E 's/.*docName=\"([^\"]*)\".*/\1/'
-draft-newton-rdap-jcr-03`
+draft=`grep docName draft-newton-rdap-jcr.xml | sed -E 's/.*docName=\"([^\"]*)\".*/\1/'`
 xml2rfc -b $draft --text draft-newton-rdap-jcr.xml
